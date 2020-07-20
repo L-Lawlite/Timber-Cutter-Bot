363f6597089fab34a7d52c683ab38a744752419e
L-Lawlite <68460205+L-Lawlite@users.noreply.github.com>
2020-07-20 21:12:20 +0530

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -10,13 +10,14 @@ module.exports = {
 	execute(message, args) {
 		const data = [];
 		const { commands } = message.client;
+		const helpEmbed = new Discord.MessageEmbed();
 
 		if (!args.length) {
 			data.push('Here\'s a list of all my commands:');
 			data.push(commands.map(command => command.name).join(', '));
 			data.push(`\nYou can send \`${prefix}help [command name]\` to get info on a specific command!`);
 
-			const helpEmbed = new Discord.MessageEmbed();
+
 			helpEmbed.setDescription(data.join(`\n`));
 
 			return message.author.send(helpEmbed, { split: true })
