185c60d66da4915a064a865b938aea1356d3cf7a
L-Lawlite <68460205+L-Lawlite@users.noreply.github.com>
2020-07-22 18:13:06 +0530

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -40,7 +40,7 @@ module.exports = {
 
 		data.push(`**Name:** ${command.name}`);
 
-		if (command.aliases) data.push(`**Aliases:** ${command.aliases.join(', ')}`);
+		if (command.aliases) data.push(`**Aliases:** ${command.aliases.join(`\n`)}`);
 		if (command.description) data.push(`**Description:** ${command.description}`);
 		if (command.usage) data.push(`**Usage:** ${prefix}${command.name} ${command.usage}`);
 
