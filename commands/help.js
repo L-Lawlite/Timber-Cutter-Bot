153378c3ae03fb4aed076893bef431c93fd1207f
L-Lawlite <68460205+L-Lawlite@users.noreply.github.com>
2020-07-22 09:28:17 +0530

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -18,7 +18,7 @@ module.exports = {
 			data.push(`\nYou can send \`${prefix}help [command name]\` to get info on a specific command!`);
 
 
-			helpEmbed.setDescription(data.join());
+			helpEmbed.setDescription(data.join(`\n`));
 
 			return message.author.send(helpEmbed)
 				.then(() => {
@@ -49,6 +49,6 @@ module.exports = {
 
 		helpEmbed.setDescription(data.join(`\n`));
 
-		message.channel.send(helpEmbed, { split: true });
+		message.channel.send(helpEmbed);
 	},
 };
