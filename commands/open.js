3084fd79203ce8400164c0397b4a32cbbb5291ab
L-Lawlite <68460205+L-Lawlite@users.noreply.github.com>
2020-07-22 18:47:55 +0530

    Create open.js

---
+++ commands/open.js
@@ -0,0 +1,18 @@
+const Discord = require('discord.js');
+
+
+module.exports{
+  name: 'open',
+	description: 'Opens channel',
+	execute(message, args) {
+		const openEmbed = new Discord.MessageEmbed()
+      .setColor('#0099ff')
+      .setTitle('**Minecraft Help Channel**')
+      .setDescription('Feel free to ask for help or advice on data packs, resource packs, or anything else related to Minecraft');
+
+      if(message.channel.name == `busy-help`){
+        message.channel.send(openEmbed);
+        message.channel.setName(`open-help`);
+      }
+	},
+};
