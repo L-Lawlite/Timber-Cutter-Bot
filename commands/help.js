62ad97fbe7950d669d5bb971a004dd9086e91c65
L-Lawlite <68460205+L-Lawlite@users.noreply.github.com>
2020-07-23 12:03:15 +0530

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -18,10 +18,9 @@ module.exports = {
 			var desc = commands.map(command => command.description);
 			for(var i=0; i<commandName.length || i < 25; i++){
 				helpEmbed.addFields(
-					{ name: commandName[i] , value1: desc[i], inline: true}
+					{ name: commandName[i] , value1: desc[i]}
 
 				)
-					helpEmbed.addBlackField(true);
 			}
 
 			helpEmbed.setFooter(`You can send \`${prefix}help [command name]\` to get info on a specific command!`);
