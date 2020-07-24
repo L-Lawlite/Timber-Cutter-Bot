0bf44c528e0d8bb428f66a11491e984b949e9464
L-Lawlite <68460205+L-Lawlite@users.noreply.github.com>
2020-07-24 13:59:08 +0530

    Update invite.js

--- commands/invite.js
+++ commands/invite.js
@@ -4,12 +4,11 @@ module.exports = {
   name: 'invite',
 	description: 'Invite bot to your channel',
 	execute(message, args) {
-		const Embed = {
-      color: 0x0099ff,
-      title: 'Invite Bot Link:',
-      description: 'click here!',
-      url: 'https://discord.com/oauth2/authorize?client_id=728842617705726026&scope=bot&permissions=8'
-    };
+    const openEmbed = new Discord.MessageEmbed()
+    .setColor('#0099ff')
+    .setTitle('**Invite Bot Link:**')
+    .setDescription('Click Here!')
+    .setURL('https://discord.com/oauth2/authorize?client_id=728842617705726026&scope=bot&permissions=8')
     message.channel.send(Embed);
 	},
 };
