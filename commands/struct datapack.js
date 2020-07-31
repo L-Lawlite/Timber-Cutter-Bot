08efa64c10425e1e9956430634f55cd5660113a1
L-Lawlite <68460205+L-Lawlite@users.noreply.github.com>
2020-07-31 09:53:25 +0530

    Update struct datapack.js

--- commands/struct datapack.js
+++ commands/struct datapack.js
@@ -4,7 +4,7 @@ module.exports = {
 	description: 'Shows Datapack Structure for 1.16',
   aliases: ['datapack'],
 	execute(message, args) {
-		fs.readFile('datapack_format.txt','uft8',function(err,data){
+		fs.readFile('datapack_format.txt','utf8',function(err,data){
 			if(err) throw err;
 			message.content.send(data);
 		});
