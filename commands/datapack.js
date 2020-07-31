99abe2e842e1c179e8c8a1d2d38bbac55db1a070
L-Lawlite <68460205+L-Lawlite@users.noreply.github.com>
2020-07-31 20:32:06 +0530

    datapack

--- commands/datapack.js
+++ commands/datapack.js
@@ -1,8 +1,7 @@
 const fs = require('fs');
 module.exports = {
-	name: 'struct datapack',
+	name: 'datapack',
 	description: 'Shows Datapack Structure for 1.16',
-  aliases: ['datapack'],
 	execute(message, args) {
 		fs.readFile('./commands/datapack_format.txt','utf8',function(err,data){
 			if(err) console.log(err);
