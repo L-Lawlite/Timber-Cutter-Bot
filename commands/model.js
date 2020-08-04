f49c07caa38770e83dc807bbfb9a56c0151670d9
L-Lawlite <68460205+L-Lawlite@users.noreply.github.com>
2020-08-04 09:11:09 +0530

    Update model.js

--- commands/model.js
+++ commands/model.js
@@ -1,4 +1,5 @@
 const Discord = require('discord.js');
+const { prefix } = require('../config.json');
 
 module.exports = {
   name:'model',
@@ -63,6 +64,8 @@ module.exports = {
         embed.setDescription(`**To ask for live help with a resource pack item model error, send SCREENSHOTS of:**\n  1. File STRUCTURE of pack leading to the folder holding the custom model.\n  2. File STRUCTURE of pack leading to the folder holding the textures.\n  3.The TEXT of the json file for the item receiving the model. (Not the model itself)\n  4.The TEXT of the top section of the model's json file.`)
         break;
       default:
+        message.reply(`invalid parameter\nValid parametrers are \`${prefix}model error\` and \`${prefix}model help\``);
+        return;
 
     }
 
