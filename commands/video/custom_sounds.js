c1296d7ef001bb92b14aa1f7b82b77076b74e766
L-Lawlite <68460205+L-Lawlite@users.noreply.github.com>
2020-08-07 09:12:24 +0530

    Update custom_sounds.js

--- commands/video/custom_sounds.js
+++ commands/video/custom_sounds.js
@@ -1,5 +1,5 @@
 module.exports = {
-	name: 'custom_sound',
+	name: 'custom_sounds',
 	description: 'Video tutorial on custom sound',
 	execute(message, args) {
 		message.channel.send('https://youtu.be/rGPxEYZW08Q');
