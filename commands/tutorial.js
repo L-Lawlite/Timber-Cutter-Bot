445b58b26562a1b77ad3231105f5ea901a152449
L-Lawlite <68460205+L-Lawlite@users.noreply.github.com>
2020-08-07 08:58:37 +0530

    tutorial video

---
+++ commands/tutorial.js
@@ -0,0 +1,50 @@
+const { prefix } = require('../config.json');
+const Discord = require('discord.js');
+const fs = require('fs');
+bot.commands = new Discord.Collection();
+const videoFiles = fs.readdirSync('./commands/video').filter(file => file.endsWith('.js'));
+
+module.exports = {
+	name: 'tutorial',
+	description: 'Links Video Tutorial',
+	aliases: ['tut'],
+	usage: '[Video Topic]',
+	execute(message, args) {
+
+    for (const file of videoFiles) {
+    	const command = require(`./commands/video/${file}`);
+    	bot.commands.set(command.name, command);
+    }
+
+    const embed = new Discord.MessageEmbed();
+
+    if (!args.length) {
+      embed.setTitle('Here\'s a list of all my commands:\n');
+      var commandName = commands.map(command => command.name);
+      var desc = commands.map(command => command.description);
+      for(var i=0; i<commandName.length; i++){
+        embed.addFields(
+          { name: commandName[i] , value: desc[i],inline: false}
+        )
+      }
+
+    return message.channel.send(embed);
+
+
+    const name = args[0].toLowerCase();
+    const command = commands.get(name) || commands.find(c => c.aliases && c.aliases.includes(name));
+
+    if (!command) {
+			return message.reply('that\'s not a valid command!');
+		}
+
+    try {
+  	   command.execute(message,args);
+     }
+     catch (error) {
+  	     console.error(error);
+  	     message.reply('there was an error trying to execute that command!');
+     }
+
+	},
+};
---
+++ commands/video/custom_sounds.js
@@ -0,0 +1,7 @@
+module.exports = {
+	name: 'custom_sound',
+	description: 'Video tutorial on custom sound',
+	execute(message, args) {
+		message.channel.send('https://youtu.be/rGPxEYZW08Q');
+	},
+};
