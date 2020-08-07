3a0d263b41fd1f2dd80dfd97936aa02e396eebeb
L-Lawlite <68460205+L-Lawlite@users.noreply.github.com>
2020-08-07 09:36:27 +0530

    Delete custom_sounds.js

--- commands/video/custom_sounds.js
+++
@@ -1,8 +0,0 @@
-
-module.exports = {
-	name: 'custom_sounds',
-	description: 'Video tutorial on custom sound',
-	execute(message, args) {
-		message.channel.send('https://youtu.be/rGPxEYZW08Q');
-	},
-};
