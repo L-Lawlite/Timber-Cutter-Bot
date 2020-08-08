b7db4e060ae611fb5d976b854d1e0c7100cefadc
L-Lawlite <68460205+L-Lawlite@users.noreply.github.com>
2020-08-08 12:43:43 +0530

    Update tutorial.js

--- commands/tutorial.js
+++ commands/tutorial.js
@@ -8,7 +8,18 @@ module.exports = {
 		const choice = args[0].toLowerCase();
 		switch (choice) {
 			case 'troubleshoot':
+			case 't':
 				message.channel.send('https://www.youtube.com/watch?v=L79L2qaH3Mw');
+				break;
+			case 'custom_mob':
+				message.channel.send('https://www.youtube.com/playlist?list=PLi7Dhu-qOl8O1ejPf2ePGYimGLsRUCD0z');
+				break;
+			case 'custom_item':
+				message.channel.send('https://www.youtube.com/playlist?list=PLi7Dhu-qOl8NQObbSPoDzJ4_7Auy-U2Gs');
+				break;
+			case 'raycasting':
+				message.channel.send(`https://youtu.be/ASVpGeLf_HU`);
+				message.channel.send('https://youtu.be/fGlJpli5cYc');
 				break;
 			default:
 
