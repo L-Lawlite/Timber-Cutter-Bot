83e81e11e25840df121fb5e4e2ee79ef3d7c8298
L-Lawlite <68460205+L-Lawlite@users.noreply.github.com>
2020-08-15 10:41:05 +0530

    Update tutorial.js

--- commands/tutorial.js
+++ commands/tutorial.js
@@ -9,7 +9,7 @@ module.exports = {
 		var videos = require("./video/videos.json")
 
 		for(i = 0; i < videos.length; i++)
-		for(j=0;j< videos.name.length;j++)
+		for(j=0;j< videos[i].name.length;j++)
 			if(choice === videos[i].name[j]){
 				message.channel.send(videos[i].url)
 			}
