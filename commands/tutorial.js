8ea6213423fb72e00afdf22524eeff91eec8dd4a
L-Lawlite <68460205+L-Lawlite@users.noreply.github.com>
2020-08-16 12:49:29 +0530

    Update tutorial.js

--- commands/tutorial.js
+++ commands/tutorial.js
@@ -3,17 +3,26 @@ const Discord = require('discord.js');
 module.exports = {
 	name: 'tutorial',
 	description: 'Links Video Tutorial',
-	aliases: ['tut','video'],
+	aliases: ['tut','video',['videos']],
 	usage: '[Video Topic]',
 	execute(message, args) {
-
+		var videos = require("./video/videos.json");
+		
 		if(!args.length)
 		{
-			
+			var helpDesc = []
+			videos.forEach(v => {
+				helpDesc.push(v.name[0]);
+				helpDesc.push(v.description);
+				helpDesc.push("\n");
+			})
+			const embed = new Discord.MessageEmbed();
+			embed.setTitle("Commands")
+			 .setDescription(helpDesc.join("\n"));
+			message.channel.send(embed);
 		}
 
 		const choice = args[0].toLowerCase();
-		var videos = require("./video/videos.json");
 		console.log(videos);
 		for(var i = 0; i < videos.length; i++)
 		for(var j=0;j< videos[i].name.length;j++)
