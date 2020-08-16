2558416c1a6398e1fc4f08ba2d92a9fed4e770f8
L-Lawlite <68460205+L-Lawlite@users.noreply.github.com>
2020-08-16 11:52:48 +0530

    Update tutorial.js

--- commands/tutorial.js
+++ commands/tutorial.js
@@ -9,6 +9,7 @@ module.exports = {
 		const choice = args[0].toLowerCase();
 		var videos = require("./video/videos.json");
 		console.log("file running");
+		console.log(videos.length);
 		for(var i = 0; i < videos.length; i++)
 		{	console.log("phase 1 passesd");
 		for(var j=0;j< videos[i].name.length;j++)
@@ -16,7 +17,6 @@ module.exports = {
 				console.log("phase 2 passed");
 				console.log(videos[i].name[j]);
 				if(choice === videos[i].name[j]){
-
 					message.channel.send(videos[i].url)
 			}
 		}}
