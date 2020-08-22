f0274649eae1bcff0a1655f51c45c5b4da5e5d13
L-Lawlite <68460205+L-Lawlite@users.noreply.github.com>
2020-08-22 14:50:56 +0530

    Update resourcepack.js

--- commands/resourcepack.js
+++ commands/resourcepack.js
@@ -1,11 +1,15 @@
 const fs = require('fs');
+const Discord = require('discord.js');
 module.exports = {
 	name: 'resourcepack',
 	description: 'Shows Resource Pack Structure for 1.16',
 	execute(message, args) {
+		const embed = new Discord.MessageEmbed()
+			.setColor('#0099ff')
+			.setDescription(`To download the template [Click Here!](https://drive.google.com/uc?export=download&id=1qbl6o0SYC4rOq0-nObG76psJ1k0K__-Z)`)
 		fs.readFile('./commands/txt/resourcepack_format.txt','utf8',function(err,data){
 			if(err) console.log(err);
-			message.channel.send(`**Resource pack Structure for 1.16:**\n\n${data}\n\nTo download the template [Click Here!](https://drive.google.com/uc?export=download&id=1qbl6o0SYC4rOq0-nObG76psJ1k0K__-Z)`);
+			message.channel.send(`**Resource pack Structure for 1.16:**\n\n${data}\n${embed}`);
 		});
 	},
 };
