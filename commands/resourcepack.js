7cec3464c496fbb68da1ea576e53b76204f44f68
L-Lawlite <68460205+L-Lawlite@users.noreply.github.com>
2020-08-22 14:54:12 +0530

    Update resourcepack.js

--- commands/resourcepack.js
+++ commands/resourcepack.js
@@ -9,7 +9,8 @@ module.exports = {
 			.setDescription(`To download the template [Click Here!](https://drive.google.com/uc?export=download&id=1qbl6o0SYC4rOq0-nObG76psJ1k0K__-Z)`)
 		fs.readFile('./commands/txt/resourcepack_format.txt','utf8',function(err,data){
 			if(err) console.log(err);
-			message.channel.send(`**Resource pack Structure for 1.16:**\n\n${data}\n${embed}`);
+			message.channel.send(`**Resource pack Structure for 1.16:**\n\n${data}`);
+			message.channel.send(embed);
 		});
 	},
 };
