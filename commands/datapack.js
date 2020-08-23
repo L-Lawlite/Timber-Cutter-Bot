40339145168601d1fad28ddf39b9e90c95412116
L-Lawlite <68460205+L-Lawlite@users.noreply.github.com>
2020-08-23 11:17:24 +0530

    Update datapack.js

--- commands/datapack.js
+++ commands/datapack.js
@@ -3,9 +3,13 @@ module.exports = {
 	name: 'datapack',
 	description: 'Shows Datapack Structure for 1.16',
 	execute(message, args) {
+		const embed = new Discord.MessageEmbed()
+			.setColor('#0099ff')
+			.setDescription(`To download the template [Click Here!]()`)
 		fs.readFile('./commands/txt/datapack_format.txt','utf8',function(err,data){
 			if(err) console.log(err);
 			message.channel.send(`**Datapack Structure for 1.16.2 and above:**\n\n${data}`);
+			message.channel.send(embed);
 		});
 	},
 };
