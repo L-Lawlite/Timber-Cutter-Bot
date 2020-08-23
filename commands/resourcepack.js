b2a7c3a1ae668918416c11d80cf46087c478689f
L-Lawlite <68460205+L-Lawlite@users.noreply.github.com>
2020-08-23 11:06:28 +0530

    Update resourcepack.js

--- commands/resourcepack.js
+++ commands/resourcepack.js
@@ -6,10 +6,10 @@ module.exports = {
 	execute(message, args) {
 		const embed = new Discord.MessageEmbed()
 			.setColor('#0099ff')
-			.setDescription(`To download the template [Click Here!](https://drive.google.com/uc?export=download&id=1qbl6o0SYC4rOq0-nObG76psJ1k0K__-Z)`)
+			.setDescription(`**Resource pack Structure for 1.16:**\n\n${data}\nTo download the template [Click Here!](https://drive.google.com/uc?export=download&id=1qbl6o0SYC4rOq0-nObG76psJ1k0K__-Z)`)
 		fs.readFile('./commands/txt/resourcepack_format.txt','utf8',function(err,data){
 			if(err) console.log(err);
-			message.channel.send(`**Resource pack Structure for 1.16:**\n\n${data}`);
+			//message.channel.send(`**Resource pack Structure for 1.16:**\n\n${data}`);
 			message.channel.send(embed);
 		});
 	},
