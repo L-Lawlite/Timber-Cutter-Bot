7bd484ad39db0ada9cda32c259b4df4f1b9c50d4
L-Lawlite <68460205+L-Lawlite@users.noreply.github.com>
2020-08-25 12:00:24 +0530

    Create faq.js

---
+++ commands/faq.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+const Discord = require('discord.js');
+module.exports = {
+	name: 'faq',
+	description: 'Some useful faq',
+	execute(message, args) {
+    var faqs = new Discord.Collection();
+    
+		});
+	},
+};
