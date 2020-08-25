a5c085596d2b8e343c47fa9e0d2a965a8026601f
L-Lawlite <68460205+L-Lawlite@users.noreply.github.com>
2020-08-25 12:38:18 +0530

    Update aec.js

---
+++ commands/faq/aec.js
@@ -0,0 +1,11 @@
+const Discord = require('discord.js');
+
+module.exports = {
+  name: 'aec',
+	description: 'AEC(area effect cloud) faq',
+	execute(message, args) {
+    const embed = new Discord.MessageEmbed()
+    .setColor('#0099ff')
+    
+	},
+};
