96027f11bf973e7c5f334a7fd4edead8ea47507e
L-Lawlite <68460205+L-Lawlite@users.noreply.github.com>
2020-08-25 12:06:29 +0530

    Update faq.js

--- commands/faq.js
+++ commands/faq.js
@@ -4,8 +4,8 @@ module.exports = {
 	name: 'faq',
 	description: 'Some useful faq',
 	execute(message, args) {
-    var faqs = new Discord.Collection();
-    const faqName = fs.readdirSync('./faq').filter(file => file.endsWith('.js'));
+    //var faqs = new Discord.Collection();
+    //const faqName = fs.readdirSync('./faq').filter(file => file.endsWith('.js'));
 
 		}
 	},
