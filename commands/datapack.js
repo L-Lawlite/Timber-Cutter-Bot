dcb13ca168c484d437a8c95f3ae6271623162b3d
L-Lawlite <68460205+L-Lawlite@users.noreply.github.com>
2020-09-20 08:57:35 +0530

    Update datapack.js

--- commands/datapack.js
+++ commands/datapack.js
@@ -2,11 +2,12 @@ const fs = require('fs');
 const Discord = require('discord.js');
 module.exports = {
 	name: 'datapack',
+	aliases: ['dpc'],
 	description: 'Shows Datapack Structure for 1.16',
 	execute(message, args) {
 		const embed = new Discord.MessageEmbed()
 			.setColor('#0099ff')
-			.setDescription(`To download the template [Click Here!](https://drive.google.com/uc?export=download&id=17PqJ4eG3vrcyIGGT_IwZz6TvIZTNlBWg)`)
+			.setDescription(`To download the template [Click Here!](https://drive.google.com/uc?export=download&id=1VE7HHIKvzixx9J-ZshZQ-Bimr8B6jCoN)`)
 		fs.readFile('./commands/txt/datapack_format.txt','utf8',function(err,data){
 			if(err) console.log(err);
 			message.channel.send(`**Datapack Structure for 1.16.2 and above:**\n\n${data}`);
