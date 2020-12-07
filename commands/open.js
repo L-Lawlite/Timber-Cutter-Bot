94a3a3028866153b88eb65bf621825ba847c4737
MbletzTheWise <75596730+MbletzTheWise@users.noreply.github.com>
2020-12-07 01:08:04 -0600

    Most I can do for today

--- commands/open.js
+++ commands/open.js
@@ -16,7 +16,7 @@ module.exports = {
           value:'Many would think this is simple, but please read this anyways.'
         },
         {
-          name:'**Give properly**:',
+          name:'**Give these properly**:',
           value:`1.What are you trying to do?\n2.What have you tried? (Send the commands and such)\n3. What's the problem? Is it invalid, or does it just not do what you want it to do?\n4. What does log say? (Optional, unless the Helper asks for it)\n\nTo learn how to properly ask a question [Click Here!](https://dontasktoask.com/)`
         }
       );
