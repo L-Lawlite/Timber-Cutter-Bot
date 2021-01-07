6831092851ae69765ce9a972e764e0f5fa507ba6
L-Lawlite <68460205+L-Lawlite@users.noreply.github.com>
2021-01-07 18:51:31 +0530

    Update open.js

--- commands/open.js
+++ commands/open.js
@@ -47,7 +47,7 @@ module.exports = {
         if(message.channel.name.startsWith(`🔑`))
         message.reply(' Just ask the question. Channel is already open.');
         else
-        message.reply('...  There\'s a time and place for everything, but not here and now.');
+        message.reply('...  There\'s a time and place for everything, but not here.');
       } 
 	},
 };
