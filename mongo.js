d8c1440c0469bc9483304c04b5c9ec0272d60b5c
L-Lawlite <68460205+L-Lawlite@users.noreply.github.com>
2021-04-12 18:06:49 +0530

    Update mongo.js

--- mongo.js
+++ mongo.js
@@ -2,6 +2,8 @@ const mongoose = require('mongoose');
 const { mongoPath } = require('./config.json');
 
 module.exports = async () => {
-    await mongoose.connect(mongoPath);
+    await mongoose.connect(mongoPath, {
+        useUnifiedTopology: true
+    });
     return mongoose;
 }
