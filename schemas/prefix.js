0dec15850279fc328d26a6798ff885e037abd036
L-Lawlite <68460205+L-Lawlite@users.noreply.github.com>
2021-04-12 18:32:45 +0530

    Create prefix.js

---
+++ schemas/prefix.js
@@ -0,0 +1,13 @@
+// const mongoose = require('mongoose');
+
+// const reqString = {
+//     type: String,
+//     required: true
+// }
+
+// const prefixSchema = mongoose.Schema({
+//     _guild_id: reqString,
+//     prefix: String
+// });
+
+// module.exports = mongoose.model('prefixes', prefixSchema);
