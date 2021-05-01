0b52e88e975ab1e15d2d9395232435d485da5e9f
L-Lawlite <68460205+L-Lawlite@users.noreply.github.com>
2021-05-01 13:51:23 +0530

    Update faq.js

--- commands/faq.js
+++ commands/faq.js
@@ -1,13 +1,22 @@
-const fs = require('fs');
 const Discord = require('discord.js');
 module.exports = {
 	name: 'faq',
 	description: 'Some useful faq',
 	execute(message, args) {
     const { faqs } = message.client;
-    console.log(faqs);
+    const faqEmbed = new Discord.MessageEmbed();
     if(!args.length){
-
+        faqEmbed.setTitle('Here\'s a list of all my faqs:\n');
+			var faqName = faqs.map(faq => faq.name);
+			var desc = faqs.map(faq => faq.description);
+			for(var i=0; i<faqName.length; i++){
+				faq.setDescription(`**${faqName[i]}**\n${desc[i]}`);
+			}
+        return message.channel.send(helpEmbed);
     }
+
+    const newArgs = args.split('faq');
+    console.log(newArgs); 
+    
 	},
 };
