d67f70664682e5d1e7b25e758c906cd0b605e233
L-Lawlite <68460205+L-Lawlite@users.noreply.github.com>
2021-05-01 13:20:41 +0530

    Update log.js

--- commands/log.js
+++ commands/log.js
@@ -1,5 +1,4 @@
 const Discord = require('discord.js');
-const image = 'https://drive.google.com/file/d/122QZSBc8MeKnZl7b7bQFOGK2d_gALg_Y/view?usp=sharing'
 module.exports = {
   name: 'log',
 	description: 'Tells how to activate log!',
@@ -7,7 +6,7 @@ module.exports = {
     const embed = new Discord.MessageEmbed()
     .setColor('#0099ff')
     .setDescription('To see the game log to debug errors in your datapack or resourcepack, do this:')
-    .setImage(image);
+    .setImage('https://imgur.com/NGo4pkg');
 
     message.channel.send(embed);
 	},
