3919aa896cec3f08541286c926ce682c044c02f5
L-Lawlite <68460205+L-Lawlite@users.noreply.github.com>
2021-05-02 19:22:25 +0530

    Update info.js

--- commands/info.js
+++ commands/info.js
@@ -5,13 +5,15 @@ module.exports = {
 	execute(message, args) {
     const { faqs } = message.client;
     const faqEmbed = new Discord.MessageEmbed();
+    var data = [];
     if(!args.length){
         faqEmbed.setTitle('Here\'s a list of all my faqs:\n');
 			var faqName = faqs.map(faq => faq.name);
 			var desc = faqs.map(faq => faq.description);
 			for(var i=0; i<faqName.length; i++){
-				faqEmbed.setDescription(`**${faqName[i]}**\n${desc[i]}`);
+				data[i] = `**${faqName[i]}**\n${desc[i]}`;
 			}
+            faqEmbed.setDescription(data.join('\n'));
         return message.channel.send(faqEmbed);
     }
 
