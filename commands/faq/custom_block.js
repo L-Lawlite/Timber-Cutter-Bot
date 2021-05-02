7e2b0debb91a639ecaf9b4fae840f9457f7d574f
L-Lawlite <68460205+L-Lawlite@users.noreply.github.com>
2021-05-02 19:03:15 +0530

    Create custom_block.js

---
+++ commands/faq/custom_block.js
@@ -0,0 +1,18 @@
+const Discord = require('discord.js');
+const {prefix} = require('.../config.json')
+
+module.exports = {
+  name: 'custom_throwable',
+	description: 'Details about custom model of throwable object',
+	execute(message, args) {
+    const embed = new Discord.MessageEmbed()
+    .setColor('#0099ff')
+    .setTitle('Custom model of Blocks')
+    .setFooter('Made by Nope#1730');
+    embed.setDescription(`Blocks, while they can technically support having custom models, a single block can only ever have one model. The only way to make a block have a custom model is to overwrite an existing block.
+
+    So to make custom blocks we have to use some workaround. There are many kind of work arounds we can do one of which is shown in \`${prefix}tutorial custom_blocks\`
+    `)
+    return message.channel.send(embed);
+	},
+};
