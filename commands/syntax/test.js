23d0c7b8b27242c4b7fce25f5c1d91a94de7ddbf
L-Lawlite <68460205+L-Lawlite@users.noreply.github.com>
2021-05-03 11:38:44 +0530

    Update test.js

---
+++ commands/syntax/test.js
@@ -0,0 +1,30 @@
+const Discord = require('discord.js');
+
+module.exports = {
+  name: 'pack.mcmeta',
+	description: 'Format of pack.mcmeta',
+	execute(message, args) {
+    const embed = new Discord.MessageEmbed()
+    .setColor('#ff4500')
+    .setTitle('Custom Model of Throwable Object');
+    embed.setDescription(`
+    \`\`\`json
+      {
+        "pack": {
+          "pack_format": <number>`+'[1]'.sup()+` ,
+          "description": "<Your Desription>"`+`[2]`.sup()`
+        }
+      }
+    \`\`\`
+    
+    `+`[1]`.sup()+` Pack Format number is:
+    4 for 1.14
+    5 for 1.15
+    6 for 1.16
+    7 for 1.17
+
+    `+`[2]`.sup()+` any string
+    `)
+    return message.channel.send(embed);
+	},
+};
