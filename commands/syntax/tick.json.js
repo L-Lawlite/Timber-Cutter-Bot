1217cdcf71372505c35556aa3f31281b4c6dc523
L-Lawlite <68460205+L-Lawlite@users.noreply.github.com>
2021-05-03 18:04:20 +0530

    Update tick.json.js

--- commands/syntax/tick.json.js
+++ commands/syntax/tick.json.js
@@ -7,7 +7,7 @@ module.exports = {
 	execute(message, args) {
     const embed = new Discord.MessageEmbed()
     .setColor('#ff4500')
-    .setTitle(`${args[0]}`);
+    .setTitle(`${args[0].toSting()}`);
     embed.setDescription(`
     \`\`\`json
     {
