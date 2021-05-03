9f3c003e4df3c23c040eaff4b321fa8e86140337
L-Lawlite <68460205+L-Lawlite@users.noreply.github.com>
2021-05-03 18:53:41 +0530

    Update tick.json.js

--- commands/syntax/tick.json.js
+++ commands/syntax/tick.json.js
@@ -5,6 +5,8 @@ module.exports = {
   aliases:['load.json','second.json'],
 	description: 'Format of tick.json or load.json',
 	execute(message, args) {
+        const { content } = message;
+        const content_list = content.split('');
     const embed = new Discord.MessageEmbed()
     .setColor('#ff4500')
     .setTitle(`tick.json or load.json`);
