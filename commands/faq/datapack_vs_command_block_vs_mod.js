54b8b969bb0bdb2ef6ac3bfcbb9e113ee0bc93ea
L-Lawlite <68460205+L-Lawlite@users.noreply.github.com>
2021-05-05 17:06:41 +0530

    Update datapack_vs_command_block_vs_mod.js

--- commands/faq/datapack_vs_command_block_vs_mod.js
+++ commands/faq/datapack_vs_command_block_vs_mod.js
@@ -9,21 +9,21 @@ module.exports = {
     .setColor('#0099ff')
     .setTitle('Comparison between Datapack, Command block and Mods')
     .setDescription(`**----========Why datapacks are better than command blocks: ========----**
-    1. Command blocks are way laggier
-    2. Datapacks have things like custom drops for entities and they have a lot more things that are not possible with only using command blocks. for example predicates and custom dimensions. 
-    3. Datapacks have functions which can be used to run a sequence of commands in a very convenient way, instead of needing a block for each command. 
-    4. Functions are only running when something triggers them, which means you can have thousands of functions sitting in your datapacks without any of them using resources unless they were triggered, which makes them way more optimized than command blocks. 
-    5. Easier way to trigger sequence of commands.
-    6. Datapack functions run all of the commands in the same tick, but the order still matters. that means you can have actions that the player cant see. for example setting a block and removing it in one function won't be seen by that player at all.
+    1. Command blocks cause more lag in general
+    2. Datapacks have things like custom drops for entities and they have a lot more things that are not possible by only using command blocks. For example predicates and custom dimensions, and loot tables. 
+    3. Datapacks have functions which are used to run a sequence of commands in a very convenient way, instead of needing a block for each command. 
+    4. Functions only run when something triggers them, so you can have thousands of functions sitting in your datapacks without any of them using resources unless they were triggered, which makes them way more optimized than command blocks. 
+    5. Easier way to trigger  sequences of commands (no need in any redstone, just one command).
+    6. Datapack functions run all of the commands in the same tick, but the order still matters. That means you can have actions that the player cant see. For example setting a block and removing it in one function won't be seen by that player at all. This is useful for things like custom names and more
     **----========Why datapacks are better than mods: ========----**
     1. Datapacks use a much easier coding language.
-    2. Datapacks don't need any extra downloads
+    2. Datapacks don't need any extra downloads - you just straight up make folders, create text files, change their extensions and you got a datapack function. 
     3. Datapacks are easier to understand
-    4. Datapacks are easier to implement into your gameplay
-    5. In servers, individuals don't need to download the pack, you can just upload the pack to the server's files
-    6. Datapacks community is currently bigger than the mod community, and the mod community is dying very fast. 
-
-    Datapacks only have one major disadvantage from mods: datapacks have way less power - they can't create custom gui, armor, crafting, mobs, blocks etc. although some of these have an alternative ways to get those. hopefully mojang will add an official way to create all of those.
+    4. Datapacks are easier to implement into your gameplay. You just drag the folder into the right path and your datapack is added to a world. 
+    5. In servers, individuals don't need to download the pack, you can just upload the pack to the server's files. 
+    6. The datapack community is currently bigger than the mod community, and the mod community is dying very fast. 
+    
+    Datapacks only have one major disadvantage from mods: Datapacks have way less power - they can't create custom gui, armor, crafting, mobs, blocks etc. although some of these have an alternative ways to get those. hopefully mojang will add an official way to create all of those in the future.
     `);
     return message.channel.send(embed);
 	},
