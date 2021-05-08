5fbf38162238b7da80d9ba995ffb7f4257c3491a
L-Lawlite <68460205+L-Lawlite@users.noreply.github.com>
2021-05-08 16:51:06 +0530

    Update datapack_is_better.js

--- commands/faq/datapack_is_better.js
+++ commands/faq/datapack_is_better.js
@@ -5,18 +5,22 @@ module.exports = {
   aliases:['datapack_vs_command_blocks','dpc_is_better','datapack_vs_mod','dpc_comparison','dpc_vs_command_blocks','dpc_vs_cmd_blocks','dpc_vs_mods'],
 	description: 'Comparison between Datapack, Command block and Mods',
 	execute(message, args) {
-    const embed = new Discord.MessageEmbed()
+    const embed1 = new Discord.MessageEmbed()
     .setColor('#0099ff')
     .setFooter('Made by GFTK#4227')
-    .setTitle('Comparison between Datapack, Command block and Mods')
-    .setDescription(`**----========Why datapacks are better than command blocks: ========----**
+    .setTitle('Datapack vs Command block');
+    const embed2 = new Discord.MessageEmbed()
+    .setColor('#0099ff')
+    .setFooter('Made by GFTK#4227')
+    .setTitle('Datapack vs Mods');
+    embed1.setDescription(`**----========Why datapacks are better than command blocks: ========----**
     1. Command blocks cause more lag in general
     2. Datapacks have things like custom drops for entities and they have a lot more things that are not possible by only using command blocks. For example predicates and custom dimensions, and loot tables. 
     3. Datapacks have functions which are used to run a sequence of commands in a very convenient way, instead of needing a block for each command. 
     4. Functions only run when something triggers them, so you can have thousands of functions sitting in your datapacks without any of them using resources unless they were triggered, which makes them way more optimized than command blocks. 
     5. Easier way to trigger  sequences of commands (no need in any redstone, just one command).
-    6. Datapack functions run all of the commands in the same tick, but the order still matters. That means you can have actions that the player cant see. For example setting a block and removing it in one function won't be seen by that player at all. This is useful for things like custom names and more
-    **----========Why datapacks are better than mods: ========----**
+    6. Datapack functions run all of the commands in the same tick, but the order still matters. That means you can have actions that the player cant see. For example setting a block and removing it in one function won't be seen by that player at all. This is useful for things like custom names and more`);
+    embed2.setDescription(`**----========Why datapacks are better than mods: ========----**
     1. Datapacks use a much easier coding language.
     2. Datapacks don't need any extra downloads - you just straight up make folders, create text files, change their extensions and you got a datapack function. 
     3. Datapacks are easier to understand
@@ -26,6 +30,7 @@ module.exports = {
     
     Datapacks only have one major disadvantage from mods: Datapacks have way less power - they can't create custom gui, armor, crafting, mobs, blocks etc. although some of these have an alternative ways to get those. hopefully mojang will add an official way to create all of those in the future.
     `);
-    return message.channel.send(embed);
+    message.channel.send(embed1)
+    message.channel.send(embed2)
 	},
 };
