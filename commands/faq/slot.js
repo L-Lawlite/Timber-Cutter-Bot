12c1db155c8b623c97ea953e50ef97d49426cbf0
L-Lawlite <68460205+L-Lawlite@users.noreply.github.com>
2021-06-20 11:06:50 +0530

    Update slot.js

--- commands/faq/slot.js
+++ commands/faq/slot.js
@@ -1,9 +1,8 @@
-
 module.exports = {
 	name: 'slot',
 	description: 'Displays slot number in Inventory',
   	aliases: ['slots'],
 	execute(message, args) {
-	  message.channel.send({files:['../images/300px-Items_slot_number.png']})
+	  message.channel.send({files:['./commands/images/300px-Items_slot_number.png']})
 	},
 };
