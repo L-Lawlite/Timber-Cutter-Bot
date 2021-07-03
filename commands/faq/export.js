3804dfa21a22373b2ff7fc2c9adb984bd18842be
WeaverSong <55251533+WeaverSong@users.noreply.github.com>
2021-07-03 00:59:17 -0600

    Moved to model command

--- commands/faq/export.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-	name: 'export',
-	description: 'Displays instructions on exporting project from Blockbench',
-  	aliases: [],
-	execute(message, args) {
-	  message.channel.send({files:['./commands/images/export.png']})
-	},
-};
